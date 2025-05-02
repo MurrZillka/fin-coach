a964ffcea4f4caf228ce09ea99e6a41cbf57ad98
Vllad Gordeev <vlladg@gmail.com>
2025-05-02 04:15:47 +0300

    Refactor Header. Install react-router-dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,27 @@
+// src/components/Header.jsx
+import { Link } from 'react-router-dom';
 import Text from './ui/Text';
-
 export default function Header() {
     return (
         <header className="bg-secondary-800 text-background p-4 shadow-md">
             <div className="max-w-7xl mx-auto flex justify-between items-center">
                 <Text variant="h1">Financial Coach</Text>
                 <nav>
-                    <a href="#" className="mx-4">
-                        <Text variant="navLink">Главная</Text>
-                    </a>
-                    <a href="#" className="mx-4">
+                    <Link to="/login" className="mx-4">
+                        <Text variant="navLink">Вход</Text>
+                    </Link>
+                    <Link to="/signup" className="mx-4">
+                        <Text variant="navLink">Регистрация</Text>
+                    </Link>
+                    <Link to="/categories" className="mx-4">
                         <Text variant="navLink">Категории</Text>
-                    </a>
-                    <a href="#" className="mx-4">
-                        <Text variant="navLink">Баланс</Text>
-                    </a>
+                    </Link>
+                    <Link to="/credits" className="mx-4">
+                        <Text variant="navLink">Доходы</Text>
+                    </Link>
+                    <Link to="/spendings" className="mx-4">
+                        <Text variant="navLink">Расходы</Text>
+                    </Link>
                 </nav>
             </div>
         </header>
