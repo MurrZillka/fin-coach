71b6ff209c056f38e83e61857f0a7a09f2972a7d
Vllad Gordeev <vlladg@gmail.com>
2025-05-03 21:35:19 +0300

    refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import {useEffect} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import useAuthStore from './stores/authStore';
 import LayoutWithHeader from './components/LayoutWithHeader';
-import TestApi from "./pages/TestApi.jsx";
-import Modal from "./components/ui/Modal.jsx";
 
 function App() {
     const { status, restoreAuth } = useAuthStore();
